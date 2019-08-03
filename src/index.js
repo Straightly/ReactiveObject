2a44ae0293770b294387f2036cf37114d82d308d
Zhi An <zan@athenahealth.com>
2019-08-03 09:28:45 -0700

    Seperate the giver and taker

--- src/index.js
+++ src/index.js
@@ -6,31 +6,7 @@ function uniqueId() {
   return '_' + Math.random().toString(36).trim();
 };
 
-function IncrementCounter(props) {
-  // Declare a new state variable, which we'll call "count"
-  let startValue = props.persistentedState;
-  if (startValue === undefined) {
-    startValue = 0;
-  }
-  const [count, setCount] = useState(startValue);
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    props.callback(props.id, count);
-    document.title = `You clicked ${count} times` + props.callback;
-  }, [count]);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-
-class InCounter extends React.Component {
+class Giver extends React.Component {
   constructor(props) {
     super(props);
     let startValue = props.persistentedState;
@@ -43,14 +19,14 @@ class InCounter extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.count !== this.state.count) {
-      this.props.callback(this.props.id, this.state.count - prevState.count);
+    if (prevState !== this.state) {
+      this.props.callback(this.props.id, this.state, prevState);
     }
   }
 
   componentDidMount() {
     if (this.props.callback) {
-      this.props.callback(this.props.id, this.state.count);
+      this.props.callback(this.props.id, this.state);
     }
   }
 
@@ -66,7 +42,32 @@ class InCounter extends React.Component {
   }
 }
 
-class Combiner extends React.Component {
+
+class Taker extends React.Component {
+  constructor(props) {
+    super(props);
+    let startValue = props.persistentedState;
+    if (startValue === undefined) {
+      startValue = 0;
+    }
+    this.state = {
+      count: startValue
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+
+class DepdendentUser extends React.Component {
   constructor(props) {
     super(props);
     let id1 = uniqueId();
@@ -77,18 +78,23 @@ class Combiner extends React.Component {
     let reducers = {};
     reducers[id1, id2] = f1;
     this.state = {
-      state1: -1, 
-      state2: -1, 
+      state1: 0, 
+      state2: 0, 
       ids: [id1, id2, id3, id4],
       reducers:reducers
     };
-
-  
   }
 
-  updateGlobalState(id, childState) {
+  updateGlobalState(id, currentChildState, prevChildSate) {
+    let childState = 0;
+    if(
+      currentChildState && 
+      currentChildState.count !== undefined && 
+      prevChildSate && 
+      prevChildSate.count !== undefined) {
+      childState = currentChildState.count - prevChildSate.count;
+    };
     if (id == this.state.ids[0] && childState > 0) {
-      let state2Value = this.state.state2 + childState;
       this.setState({
         state1: this.state.state1 + childState, 
         state2: this.state.state2 + childState});
@@ -101,8 +107,8 @@ class Combiner extends React.Component {
   render() {
       return (
         <div>
-          <InCounter        id={this.state.ids[0]} callback={(id, count) => this.updateGlobalState(id, count)} startValue={this.state.state1}/>
-          <InCounter        id={this.state.ids[1]} callback={(id, count) => this.updateGlobalState(id, count)} startValue={this.state.state2}/>
+          <Giver        id={this.state.ids[0]} callback={(id, prevState, curState) => this.updateGlobalState(id, prevState, curState)} startValue={this.state.state1}/>
+          <Taker        id={this.state.ids[1]} callback={(id, prevState, curState) => this.updateGlobalState(id, prevState, curState)} startValue={this.state.state2}/>
           <p/>
           <p>Total is {this.state.state1} + {this.state.state2}</p>
         </div>
@@ -111,7 +117,7 @@ class Combiner extends React.Component {
 }//class
 
 ReactDOM.render(
-  <Combiner/>,
+  <DepdendentUser/>,
   document.getElementById("root"),
   null
 );
